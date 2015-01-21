efe6b7fb86a797ee49650ee8c0308d56c5c4aa1b
zule8 <254487002@qq.com>
2015-01-21 21:19:31 +0800

    构造了项目框架
    刚刚开始，加油

---
+++ tutu/src/main/java/com/zuleba/TuTuConstants.java
@@ -0,0 +1,11 @@
+/**
+* @作者：zhutongtao
+* @时间：2015年1月21日 下午9:09:49
+* @项目名：tutu
+*/
+package com.zuleba;
+
+public class TuTuConstants {
+
+}
+
---
+++ tutu/src/main/java/com/zuleba/controller/BaseController.java
@@ -0,0 +1,14 @@
+/**
+* @作者：zhutongtao
+* @时间：2015年1月21日 下午9:07:37
+* @项目名：tutu
+*/
+package com.zuleba.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BaseController {
+
+}
+
